b3a55e8def947b684589540608a7a3604448cc29
Aryansr25 <aryanshankarath@gmail.com>
2025-09-06 19:23:00 +0530

    made top 50 recommendation

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import pickle
 
-app=Flask(__name__)
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name=popular_df['Book-Title'].to_list(),
+                           author_name=popular_df['Book-Author'].to_list(),
+                           image=popular_df['Image-URL-M'].to_list(),
+                           votes=popular_df['num_ratings'].to_list(),
+                           ratings=popular_df['avg_ratings'].to_list()
+                           )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
